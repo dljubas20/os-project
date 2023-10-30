f6917b8a1b8083b66b35aaae063f1d2cde664169
Dario Ljubas <darioljubas33@gmail.com>
2023-10-30 09:52:50 +0100

    Added MatBlazor package for UI

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using MatBlazor;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using os_project.Data;
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddMatBlazor();
 
 var app = builder.Build();
 
