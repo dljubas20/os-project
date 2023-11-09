4c4fed50d832ccac40046acfd2c6b6c5c52ddeb0
Dario Ljubas <darioljubas33@gmail.com>
2023-11-10 00:51:21 +0100

    Added MudBlazor package, removed MatBlazor package

--- Program.cs
+++ Program.cs
@@ -1,13 +1,13 @@
-using MatBlazor;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddMatBlazor();
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
