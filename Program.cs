1f9a464a4a54050a5dde5eca0dd2a2e6f07c1208
Dario Ljubas <darioljubas33@gmail.com>
2023-11-12 17:40:42 +0100

    Added dependency injection of EncryptionRepository

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using MudBlazor.Services;
+using os_project.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddMudServices();
+builder.Services.AddSingleton<IEncryptionRepository, EncryptionRepository>();
 
 var app = builder.Build();
 
